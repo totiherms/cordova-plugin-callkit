46e1496058dcdb6beb74d09d52945218588b6437
totiherms <toti@herms.com>
2021-06-01 16:03:48 +0200

    android not make calls, only receive

--- src/android/CordovaCall.java
+++ src/android/CordovaCall.java
@@ -83,7 +83,7 @@ public class CordovaCall extends CordovaPlugin {
         }
         if(android.os.Build.VERSION.SDK_INT >= 23) {
           phoneAccount = new PhoneAccount.Builder(handle, appName)
-                   .setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER)
+                   .setCapabilities(PhoneAccount.CAPABILITY_CALL_SUBJECT)
                    .build();
           tm.registerPhoneAccount(phoneAccount);          
         }
@@ -194,7 +194,7 @@ public class CordovaCall extends CordovaPlugin {
             }
             if(android.os.Build.VERSION.SDK_INT >= 23) {
               phoneAccount = new PhoneAccount.Builder(handle, appName)
-                   .setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER)
+                   .setCapabilities(PhoneAccount.CAPABILITY_CALL_SUBJECT)
                    .build();
               tm.registerPhoneAccount(phoneAccount);
             }
